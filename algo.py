c72984c5494d250f2286df107f494d44c6261525
Atif Pathan <atifkhan.p1@gmail.com>
2024-03-03 16:32:46 -0500

    Merge smalled functions into the main three functions and format a little

--- algo.py
+++ algo.py
@@ -1,13 +1,8 @@
 import numpy as np
-from get_risk_free import get_risk_free_rate
+import pandas as pd
+from fredapi import Fred
 from data import get_data
-from get_log_norm_return import calculateLogReturns
-from get_covariance import calculateCovarianceMatrix
-from get_correlation import get_corr
-from get_std_dev import std_dev
-from get_exp_return import expected_return
-from get_sharpe_ratio import sharpe_ratio
-from get_allocation import allocation
+from scipy.optimize import minimize
 
 def run_algo(tickers, start_date, end_date, risk_tolerance, investment_amount, min_bound, max_bound):
 
@@ -21,8 +16,7 @@ def run_algo(tickers, start_date, end_date, risk_tolerance, investment_amount, m
     else:
         # Define initial weights to be equal
         initial_weights = np.array([1/adj_close_prices_df.shape[1]]*adj_close_prices_df.shape[1]) 
-        # print head
-        print(adj_close_prices_df)
+        print(adj_close_prices_df) # print head
         print(f"risk_free_rate = {risk_free_rate}")
         log_ret = calculateLogReturns(adj_close_prices_df)
         cov = calculateCovarianceMatrix(log_ret)
@@ -30,3 +24,34 @@ def run_algo(tickers, start_date, end_date, risk_tolerance, investment_amount, m
 
     # for now, return data not the calculated results to App
     return adj_close_prices_df, risk_free_rate, log_ret, cov, corr
+
+
+def calculateLogReturns(adj_close):
+    log_ret = np.log(adj_close / adj_close.shift(1))
+    log_ret = log_ret.dropna()  # Drop missing values
+    return log_ret
+
+def calculateCovarianceMatrix(log_ret):
+    return log_ret.cov() * 252 # using 252 as the trading days in a year
+
+def get_corr(df):
+    return df.corr()
+
+def std_dev(w, cov):
+    deviation = np.dot(w.T, np.dot(cov, w))
+    return np.sqrt(deviation)
+
+def expected_return(w, log_ret):
+    return np.sum(log_ret.mean() * w) * 252
+
+def sharpe_ratio(exp_return, std, risk_free_rate):    
+    return (exp_return - risk_free_rate)/std
+
+def allocation(capital, weights):
+    return [w * capital for w in weights]
+
+def get_risk_free_rate():
+    fr = Fred(api_key='9b01535ec1ca714437ec3bed84a37f77') # free api key
+    treasury = fr.get_series_latest_release('GS10')/100
+    risk = treasury.iloc[-1]
+    return risk
--- data.py
+++ data.py
@@ -1,13 +1,13 @@
 import pandas as pd
-from get_data_yfinance import get_data_yfinance
-from get_data_yahoo_fin import get_data_yahoo_fin
-from get_data_tiingo import get_data_tiingo
+import yfinance as yf
+from yahoo_fin.stock_info import get_data
+from tiingo import TiingoClient
+from tiingo.restclient import RestClientError
 
 def get_data(tickers, start_date, end_date):
     
     df = pd.DataFrame()
-
-    try:
+    try: # try to get data from yfinance
         print("Attempting to fetch data using yfinance...")
         df = get_data_yfinance(tickers, start_date, end_date)
         
@@ -18,7 +18,7 @@ def get_data(tickers, start_date, end_date):
     except Exception as e:
         print(f"yfinance failed: {e}. Attempting to fetch data using yahoo_fin...")
         
-    try:
+    try: # did not return so try using yahoo_fin
         df = get_data_yahoo_fin(tickers, start_date, end_date)
         if not df.empty:
             return df
@@ -27,7 +27,7 @@ def get_data(tickers, start_date, end_date):
     except Exception as e:
         print(f"yahoo_fin failed: {e}. Attempting to fetch data using Tiingo...")
         
-    try:
+    try: # did not work so try tiingo
         df = get_data_tiingo(tickers, start_date, end_date)
         if not df.empty:
             return df
@@ -35,7 +35,59 @@ def get_data(tickers, start_date, end_date):
             raise ValueError("No data fetched with Tiingo.")
     except Exception as e:
         print(f"Tiingo failed: {e}. No more data sources to attempt.")
-        return df
+        # it will still return a df, will just be empty
+        return df 
 
 
-#insert functions directly into file
+def get_data_yfinance(tickers, start_date, end_date, res="1d"):
+    df = pd.DataFrame()
+    for ticker in tickers:
+        data = yf.download(ticker, start=start_date, end=end_date, interval=res)["Adj Close"]
+        if data.isnull().all() or data.isna().all():
+            continue
+        else:
+            df.index.name = None
+            df[ticker] = data
+    return df
+
+
+def get_data_yahoo_fin(tickers, start_date, end_date, res="1d"):
+    df = pd.DataFrame()
+    for ticker in tickers:
+        try:
+            # have to swap / with - for dates
+            data = get_data(ticker, start_date=start_date.replace("-", "/"), end_date=end_date.replace("-", "/"), interval=res)["adjclose"]
+            if data.isnull().all() or data.isna().all():
+                continue
+            else:
+                df.index.name = None
+                df[ticker] = data
+        except (KeyError, AssertionError):
+            continue
+    return df
+
+
+def get_data_tiingo(tickers, start_date, end_date, res='daily', metric='adjClose'):
+    # use default daily resolution and adjClose as the metric to be pulled
+    config = {}
+    config['session'] = True
+    config['api_key'] = "a9716d1a5f9f2178ea0489147199df649d296e86" # free api key
+    client = TiingoClient(config)
+    
+    df = pd.DataFrame()
+    for ticker in tickers:
+        try:
+            data = client.get_dataframe(ticker, frequency=res, metric_name=metric,
+                                        startDate=start_date, endDate=end_date)
+            if data.isnull().all() or data.isna().all():
+                continue
+            else:
+                df[ticker] = data
+        except RestClientError:
+            print(f"Ticker '{ticker}' not found. Skipping...")
+            continue
+    # some formatting for output dataframe
+    df.index = pd.to_datetime(df.index)
+    df.index = df.index.strftime('%Y-%m-%d')
+    df.index.name = None
+    return df
